145629c52241fe6228ce48f207d92956bedcd5e8
user <user@Dmitry>
2020-12-29 16:49:37 +0300

    Список должностей.

--- user/create.php
+++ user/create.php
@@ -76,16 +76,22 @@ if($user_create_submit !== null) {
             ?>
             <div class="row">
                 <div class="col-12 col-md-6 col-lg-4">
-                    <div class="d-flex justify-content-between mb-2">
-                        <div class="p-1">
-                            <h1>Новый пользователь</h1>
-                        </div>
-                        <div class="p-1">
-                            <a href="<?=APPLICATION ?>/user/" class="btn btn-outline-dark"><i class="fas fa-undo-alt"></i>&nbsp;Отмена</a>
-                        </div>
-                    </div>
-                    <hr/>
+                    <a href="<?=APPLICATION ?>/user/"><i class="fas fa-chevron-left"></i>&nbsp;Назад</a>
+                    <h1>Добавление сотрудника</h1>
                     <form method="post">
+                        <div class="form-group">
+                            <select id="role_id" name="role_id" class="form-control" required="required">
+                                <option value="">ВЫБЕРИТЕ ДОЛЖНОСТЬ</option>
+                                <?php
+                                $roles = (new Grabber('select id, local_name from role order by local_name'))->result;
+                                foreach ($roles as $role) {
+                                    $id = $role['id'];
+                                    $local_name = $role['local_name'];
+                                    echo "<option value='$id'>$local_name</option>";
+                                }
+                                ?>
+                            </select>
+                        </div>
                         <div class="form-group">
                             <label for="username">Логин</label>
                             <input type="text" id="username" name="username" class="form-control<?=$username_valid ?>" value="<?= isset($username) ? $username : '' ?>" required="required" autocomplete="off"/>
@@ -107,7 +113,7 @@ if($user_create_submit !== null) {
                             <div class="invalid-feedback">Неправильный формат E-Mail</div>
                         </div>
                         <div class="form-group">
-                            <button type="submit" class="btn btn-outline-dark" id="user_create_submit" name="user_create_submit">Сохранить</button>
+                            <button type="submit" class="btn btn-dark" id="user_create_submit" name="user_create_submit">Создать</button>
                         </div>
                     </form>
                 </div>
