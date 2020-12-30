89773842395d68275a8a7b9fe7fa6e6b5333a378
user <user@Dmitry>
2020-12-30 16:35:09 +0300

    Создание поставщика.

--- supplier/create.php
+++ supplier/create.php
@@ -29,13 +29,15 @@ if($supplier_create_submit !== null || $film_type_create_submit !== null) {
         $executer = new Executer("insert into supplier (name) values ('$name')");
         $error_message = $executer->error;
         $id = $executer->insert_id;
-        
+
         if($error_message == '') {
-            if($supplier_create_submit != null) {
+            if($supplier_create_submit !== null) {
+                echo 'supplier_create_submit';
                 header('Location: '.APPLICATION."/supplier/");
             }
             
             if($film_type_create_submit !== null) {
+                echo 'film_type_create_submit';
                 header('Location: '.APPLICATION."/supplier/details=$id");
             }
         }
@@ -46,7 +48,38 @@ if($supplier_create_submit !== null || $film_type_create_submit !== null) {
 <html>
     <head>
         <?php
-        include '../include/';
+        include '../include/head.php';
         ?>
     </head>
+    <body>
+        <?php
+        include '../include/header.php';
+        ?>
+        <div class="container-fluid">
+            <?php
+            if(isset($error_message) && $error_message != '') {
+                echo "<div class='alert alert-danger'>$error_message</div>";
+            }
+            ?>
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-4">
+                    <a href="<?=APPLICATION ?>/supplier/"><i class="fas fa-chevron-left"></i>&nbsp;Назад</a>
+                    <h1>Добавление поставщика</h1>
+                    <form method="post">
+                        <div class="form-group">
+                            <label for="name">Название поставщика</label>
+                            <input type="text" id="name" name="name" class="form-control<?=$name_valid ?>" value="<?= filter_input(INPUT_POST, 'name') ?>" required="required"/>
+                            <div class="invalid-feedback">Название поставщика обязательно</div>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-outline-dark" id="film_type_create_submit" name="film_type_create_submit"><i class="fas fa-plus"></i>&nbsp;Добавить марку пленки</button>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-dark" id="supplier_create_submit" name="supplier_create_submit">Создать поставщика</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
