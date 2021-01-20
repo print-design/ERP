14450cf6d5a4c53be042d85b3dd00a408afef348
dmitry panteleichev <dimatver@bk.ru>
2021-01-20 22:05:19 +0300

    Смена пароля.

--- personal/password.php
+++ personal/password.php
@@ -70,15 +70,8 @@ if($password_change_submit !== null) {
             ?>
             <div class="row">
                 <div class="col-12 col-md-6 col-lg-4">
-                    <div class="d-flex justify-content-between mb-2">
-                        <div class="p-1">
-                            <h1>Смена пароля</h1>
-                        </div>
-                        <div class="p-1">
-                            <a href="<?=APPLICATION ?>/personal/" class="btn btn-outline-dark"><i class="fas fa-undo-alt"></i>&nbsp;Отмена</a>
-                        </div>
-                    </div>
-                    <hr/>
+                    <a href="<?=APPLICATION ?>/personal/"><i class="fas fa-chevron-left"></i>&nbsp;Назад</a>
+                    <h1>Смена пароля</h1>
                     <form method="post">
                         <div class="form-group">
                             <label for="old_password">Текущий пароль</label>
@@ -96,7 +89,7 @@ if($password_change_submit !== null) {
                             <div class="invalid-feedback">Новый пароль и его подтверждение не совпадают</div>
                         </div>
                         <div class="form-group">
-                            <button type="submit" class="btn btn-outline-dark" id="password_change_submit" name="password_change_submit">Сменить</button>
+                            <button type="submit" class="btn btn-dark" id="password_change_submit" name="password_change_submit">Сменить</button>
                         </div>
                     </form>
                 </div>
