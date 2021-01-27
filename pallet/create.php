c39b214b740ab906c9b9f85e096bc55dab01f90d
dmitry panteleichev <dimatver@bk.ru>
2021-01-28 02:02:05 +0300

    Новый паллет.

--- pallet/create.php
+++ pallet/create.php
@@ -1,2 +1,37 @@
 <?php
+include '../include/topscripts.php';
+
+// Авторизация
+if(!IsInRole(array('admin', 'dev', 'technologist', 'storekeeper'))) {
+    header('Location: '.APPLICATION.'/unauthorized.php');
+}
+
+// Валидация формы
+
+// Обработка отправки формы
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+        include '../include/head.php';
+        ?>
+    </head>
+    <body>
+        <?php
+        include '../include/header.php';
+        ?>
+        <div class="container-fluid">
+            <?php
+            if(!empty($error_message)) {
+                echo "<div class='alert alert-danger>$error_message</div>";
+            }
+            ?>
+            <div class="backlink">
+                <a href="<?=APPLICATION ?>/pallet/"><i class="fas fa-chevron-left"></i>&nbsp;Назад</a>
+            </div>
+            <h1>Новый паллет</h1>
+            <h2>Паллет №</h2>
+        </div>
+    </body>
+</html>
