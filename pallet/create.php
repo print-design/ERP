281a1c0b76c11cae718cc103c2b740c9e8c662da
dmitry panteleichev <dimatver@bk.ru>
2021-01-28 16:34:43 +0300

    Создание паллета

--- pallet/create.php
+++ pallet/create.php
@@ -9,6 +9,10 @@ if(!IsInRole(array('admin', 'dev', 'technologist', 'storekeeper'))) {
 // Валидация формы
 
 // Обработка отправки формы
+
+// Получение данных
+$id = 0;
+$date = date("d.m.Y");
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,17 +25,34 @@ if(!IsInRole(array('admin', 'dev', 'technologist', 'storekeeper'))) {
         <?php
         include '../include/header.php';
         ?>
-        <div class="container-fluid">
+        <div class="container-fluid" style="padding-left: 40px;">
             <?php
             if(!empty($error_message)) {
                 echo "<div class='alert alert-danger>$error_message</div>";
             }
             ?>
-            <div class="backlink">
+            <div class="backlink" style="margin-bottom: 56px;">
                 <a href="<?=APPLICATION ?>/pallet/"><i class="fas fa-chevron-left"></i>&nbsp;Назад</a>
             </div>
-            <h1>Новый паллет</h1>
-            <h2>Паллет №</h2>
+            <h1 style="font-size: 32px; line-height: 48px; font-weight: 600; margin-bottom: 20px;">Новый паллет</h1>
+            <h2 style="font-size: 24px; line-height: 32px; font-weight: 600; margin-bottom: 20px;">Паллет № <?=$id ?> от <?=$date ?></h2>
+            <div style="width: 423px;">
+                <form method="post">
+                    <input type="hidden" />
+                    <div class="form-group">
+                        <label for="supplier_id">Поставщик</label>
+                        <select id="supplier_id" name="supplier_id" class="form-control" required="required">
+                            <option value="">Выберите поставщика</option>
+                        </select>
+                        <div class="invalid-feedback">Поставщик обязательно</div>
+                    </div>
+                    <div class="form-group">
+                        <label for="id_from_supplier">ID паллета от поставщика</label>
+                        <input type="text" id="id_from_supplier" name="id_from_supplier" class="form-control" placeholder="Введите ID" required="required" />
+                        <div class="invalid-feedback">ID паллета от поставщика обязательно</div>
+                    </div>
+                </form>
+            </div>
         </div>
         <?php
         include '../include/footer.php';
