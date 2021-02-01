c4c30a4135fcc74d61c254dd955ec0b5c3c33a0c
user <user@Dmitry>
2021-02-01 15:25:27 +0300

    Фильтр.

--- pallet/index.php
+++ pallet/index.php
@@ -30,7 +30,7 @@ if(!IsInRole(array('admin', 'dev', 'technologist', 'storekeeper'))) {
                 </div>
                 <div class="p-1">
                     <a href="create.php" class="btn btn-outline-dark" style="margin-right: 12px; padding-left: 33px; padding-right: 44px;"><i class="fas fa-plus" style="font-size: 10px; margin-right: 18px;"></i>Новый паллет</a>
-                    <a class="btn btn-outline-dark" style="padding-left: 14px; padding-right: 42px; padding-bottom: 14px; padding-top: 15px;"><img src="../images/icons/filter.svg" style="margin-right: 20px;" />Фильтр</a>
+                    <button class="btn btn-outline-dark" data-toggle="modal" data-target="#filterModal" data-text="Фильтр" style="padding-left: 14px; padding-right: 42px; padding-bottom: 14px; padding-top: 14px;"><img src="../images/icons/filter.svg" style="margin-right: 20px;" />Фильтр</button>
                 </div>
             </div>
             <table class="table">
@@ -99,5 +99,21 @@ if(!IsInRole(array('admin', 'dev', 'technologist', 'storekeeper'))) {
         <?php
         include '../include/footer.php';
         ?>
+        <div class="modal fade" id="filterModal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <button type="button" class="close" data-dismiss="modal" style="position: absolute; right: 15px; top: 5px;">&times;</button>
+                    <h1>Фильтр</h1>
+                    <h2>Статус</h2>
+                    <form method="get">
+                        <div class="form-group form-check">
+                            <label class="form-check-label">
+                                <input type="checkbox" class="form-check-input" id="chkPrint" name="chkPrint" /> В ПЕЧАТЬ
+                            </label>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
