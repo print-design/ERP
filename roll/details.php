4e027e2b61eeae0f4409a43c5d501c5d69355228
user <user@Dmitry>
2021-02-03 17:01:53 +0300

    Просмотр рулона.

--- roll/details.php
+++ roll/details.php
@@ -2,9 +2,38 @@
 include '../include/topscripts.php';
 
 // Авторизация
-if(!LoggedIn()) {
+if(!IsInRole(array('admin', 'dev', 'technologist', 'storekeeper'))) {
     header('Location: '.APPLICATION.'/unauthorized.php');
 }
+
+// Если не задано значение id, перенаправляем на список
+if(empty(filter_input(INPUT_GET, 'id'))) {
+    header('Location: '.APPLICATION.'/roll/');
+}
+
+// Получение данных
+$id = filter_input(INPUT_GET, 'id');
+$sql = "select r.inner_id, r.date, r.storekeeper_id, u.last_name, u.first_name, r.supplier_id, r.id_from_supplier, r.film_brand_id, r.width, r.thickness, r.length, "
+        . "r.net_weight, r.cell, r.manager_id, r.status_id, r.comment "
+        . "from roll r inner join user u on r.storekeeper_id = u.id "
+        . "where r.id=$id";
+
+$row = (new Fetcher($sql))->Fetch();
+$inner_id = $row['inner_id'];
+$date = $row['date'];
+$storekeeper_id = $row['storekeeper_id'];
+$storekeeper = $row['last_name'].' '.$row['first_name'];
+$supplier_id = $row['supplier_id'];
+$id_from_supplier = $row['id_from_supplier'];
+$film_brand_id = $row['film_brand_id'];
+$width = $row['width'];
+$thickness = $row['thickness'];
+$length = $row['length'];
+$net_weight = $row['net_weight'];
+$cell = $row['cell'];
+$manager_id = $row['manager_id'];
+$status_id = $row['status_id'];
+$comment = $row['comment'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,19 +46,138 @@ if(!LoggedIn()) {
         <?php
         include '../include/header.php';
         ?>
-        <div class="container-fluid">
+        <div class="container-fluid" style="padding-left: 40px;">
             <?php
-            if(isset($error_message) && $error_message != '') {
-               echo "<div class='alert alert-danger'>$error_message</div>";
+            if(!empty($error_message)) {
+                echo "<div class='alert alert-danger>$error_message</div>";
             }
             ?>
-            <div class="btn-group">
-                <a href="<?=APPLICATION ?>/rolls/" class="btn btn-outline-dark"><i class="fas fa-undo"></i>&nbsp;Назад</a>
-           </div>
-            <h1>Информация о ролике</h1>
+            <div class="backlink" style="margin-bottom: 56px;">
+                <a href="<?=APPLICATION ?>/roll/"><i class="fas fa-chevron-left"></i>&nbsp;Назад</a>
+            </div>
+            <h1 style="font-size: 24px; line-height: 32px; fon24pxt-weight: 600; margin-bottom: 20px;">Информация о рулоне № <?=$inner_id ?> от <?= (DateTime::createFromFormat('Y-m-d', $date))->format('d.m.Y') ?></h1>
+            <h2 style="font-size: 24px; line-height: 32px; font-weight: 600; margin-bottom: 20px;">ID <?=$id_from_supplier ?></h2>
+            <form>
+                <div style="width: 423px;">
+                    <div class="form-group">
+                        <label for="storekeeper">Принят кладовщиком</label>
+                        <p id="storekeeper"><?=$storekeeper ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label for="supplier_id">Поставщик</label>
+                        <select id="supplier_id" name="supplier_id" class="form-control" disabled="disabled">
+                            <option value="">Выберите поставщика</option>
+                            <?php
+                            $suppliers = (new Grabber("select id, name from supplier order by name"))->result;
+                            foreach ($suppliers as $supplier) {
+                                $id = $supplier['id'];
+                                $name = $supplier['name'];
+                                $selected = '';
+                                if($supplier_id == $supplier['id']) $selected = " selected='selected'";
+                                echo "<option value='$id'$selected>$name</option>";
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="id_from_supplier">ID рулона от поставщика</label>
+                        <input type="text" id="id_from_supplier" name="id_from_supplier" value="<?= $id_from_supplier ?>" class="form-control" placeholder="Введите ID" disabled="disabled" />
+                    </div>
+                    <div class="form-group">
+                        <label for="film_brand_id">Марка пленки</label>
+                        <select id="film_brand_id" name="film_brand_id" class="form-control" disabled="disabled">
+                            <option value="">Выберите марку</option>
+                            <?php
+                            $film_brands = (new Grabber("select id, name from film_brand where supplier_id = $supplier_id"))->result;
+                            foreach ($film_brands as $film_brand) {
+                                $id = $film_brand['id'];
+                                $name = $film_brand['name'];
+                                $selected = '';
+                                if($film_brand_id == $film_brand['id']) $selected = " selected='selected'";
+                                echo "<option value='$id'$selected>$name</option>";
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="row">
+                        <div class="col-6 form-group">
+                            <label for="width">Ширина</label>
+                            <input type="text" id="width" name="width" value="<?= $width ?>" class="form-control int-only" placeholder="Введите ширину" disabled="disabled" />
+                        </div>
+                        <div class="col-6 form-group">
+                            <label for="thickness">Толщина</label>
+                            <select id="thickness" name="thickness" class="form-control" disabled="disabled">
+                                <option value="">Выберите толщину</option>
+                                <option value="8">8</option>
+                                <?php
+                                for($i=10; $i<=80; $i=$i+10) {
+                                    $selected = '';
+                                    if($thickness == $i) $selected = " selected='selected'";
+                                    echo "<option value='$i'$selected>$i</option>";
+                                }
+                                ?>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6 form-group">
+                            <label for="length">Длина</label>
+                            <input type="text" id="length" name="length" value="<?= $length ?>" class="form-control int-only" placeholder="Введите длину" disabled="disabled" />
+                        </div>
+                        <div class="col-6 form-group">
+                            <label for="net_weight">Масса нетто</label>
+                            <input type="text" id="net_weight" name="net_weight" value="<?= $net_weight ?>" class="form-control int-only" placeholder="Введите массу нетто" disabled="disabled" />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6 form-group"></div>
+                        <div class="col-6 form-group">
+                            <label for="cell">Ячейка на складе</label>
+                            <input type="text" id="cell" name="cell" value="<?= $cell ?>" class="form-control" placeholder="Введите ячейку" disabled="disabled" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="manager_id">Менеджер</label>
+                        <select id="manager_id" name="manager_id" class="form-control" disabled="disabled">
+                            <option value="">Выберите менеджера</option>
+                            <?php
+                            $managers = (new Grabber("select u.id, u.first_name, u.last_name from user u inner join role r on u.role_id = r.id where r.name in ('manager', 'seniormanager') order by u.last_name"))->result;
+                            foreach ($managers as $manager) {
+                                $id = $manager['id'];
+                                $first_name = $manager['first_name'];
+                                $last_name = $manager['last_name'];
+                                $selected = '';
+                                if($manager_id == $manager['id']) $selected = " selected='selected'";
+                                echo "<option value='$id'$selected>$last_name $first_name</option>";
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="status_id">Статус</label>
+                        <select id="status_id" name="status_id" class="form-control" disabled="disabled" style="background-color: #8B90A0; color: white;">
+                            <option value="">ВЫБРАТЬ СТАТУС</option>
+                            <?php
+                            $statuses = (new Grabber("select s.id, s.name from roll_status s order by s.name"))->result;
+                            foreach ($statuses as $status) {
+                                $id = $status['id'];
+                                $name = $status['name'];
+                                $selected = '';
+                                if($status_id == $status['id']) $selected = " selected='selected'";
+                                echo "<option value='$id'$selected>$name</option>";
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="comment">Комментарий</label>
+                        <textarea id="comment" name="comment" rows="4" class="form-control" disabled="disabled"><?= htmlentities($comment) ?></textarea>
+                    </div>
+                </div>
+            </form>
         </div>
         <?php
-        include 'include/footer.php';
+        include '../include/footer.php';
         ?>
     </body>
 </html>
---
+++ roll/sticker.php
@@ -0,0 +1,160 @@
+<?php
+include '../include/topscripts.php';
+
+// Авторизация
+if(!IsInRole(array('admin', 'dev', 'technologist', 'storekeeper'))) {
+    header('Location: '.APPLICATION.'/unauthorized.php');
+}
+
+// Получение данных
+$inner_id = filter_input(INPUT_POST, 'inner_id');
+$date = filter_input(INPUT_POST, 'date');
+$supplier_id = filter_input(INPUT_POST, 'supplier_id');
+$id_from_supplier = filter_input(INPUT_POST, 'id_from_supplier');
+$film_brand_id = filter_input(INPUT_POST, 'film_brand_id');
+$width = filter_input(INPUT_POST, 'width');
+$thickness = filter_input(INPUT_POST, 'thickness');
+$length = filter_input(INPUT_POST, 'length');
+$net_weight = filter_input(INPUT_POST, 'net_weight');
+$cell = filter_input(INPUT_POST, 'cell');
+$manager_id = filter_input(INPUT_POST, 'manager_id');
+$status_id = filter_input(INPUT_POST, 'status_id');
+$comment = filter_input(INPUT_POST, 'comment');
+
+$supplier = '';
+$film_brand = '';
+$manager = '';
+$status = '';
+
+if(!empty($supplier_id) || !empty($film_brand_id) || !empty($manager_id) || !empty($status_id)) {
+    $sql = '';
+    
+    if(!empty($supplier_id)) {
+        $sql .= "s.name supplier"; 
+    }
+    
+    if(!empty($film_brand_id)) {
+        if(!empty($sql)) $sql .= ", ";
+        $sql .= "fb.name film_brand";
+    }
+    
+    if(!empty($manager_id)) {
+        if(!empty($sql)) $sql .= ", ";
+        $sql .= "u.last_name, u.first_name";
+    }
+    
+    if(!empty($status_id)) {
+        if(!empty($sql)) $sql .= ", ";
+        $sql .= "st.name status";
+    }
+    
+    $sql = "select $sql from roll r "
+            . "left join supplier s on r.supplier_id = s.id "
+            . "left join film_brand fb on r.film_brand_id = fb.id "
+            . "left join user u on r.manager_id = u.id "
+            . "left join roll_status st on r.status_id = st.id";
+    
+    $row = (new Fetcher($sql))->Fetch();
+    if(!empty($row['supplier'])) $supplier = $row['supplier'];
+    if(!empty($row['film_brand'])) $film_brand = $row['film_brand'];
+    if(!empty($row['last_name']) || !empty($row['first_name'])) $manager = $row['last_name'].' '.$row['first_name'];
+    if(!empty($row['status'])) $status = $row['status'];
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+        include '../include/head.php';
+        ?>
+    </head>
+    <body class="print">
+        <div style="width: 400px; margin-left: 20px;">
+            <h1 style="font-size: 26px; margin-top: 10px; margin-bottom: 30px;">Рулон №<?=$inner_id ?> от <?=$date ?></h1>
+            <table class="table table-bordered">
+                <tbody>
+                    <tr>
+                        <td colspan="2">
+                            <strong>Поставщик:</strong><br/>
+                            <?=$supplier ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <strong>ID от поставщика:</strong><br/>
+                            <?=$inner_id ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <strong>Марка пленки</strong><br/>
+                            <?=$film_brand ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong>Ширина</strong><br/>
+                            <?=$width ?>
+                        </td>
+                        <td>
+                            <strong>Толщина</strong><br/>
+                            <?=$thickness ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong>Длина</strong><br/>
+                            <?=$length ?>
+                        </td>
+                        <td>
+                            <strong>Масса нетто</strong><br/>
+                            <?=$net_weight ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <strong>Ячейка на складе</strong><br/>
+                            <?=$cell ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <strong>Менеджер</strong><br/>
+                            <?=$manager ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <strong>Статус</strong><br/>
+                            <?=$status ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <strong>Комментарий</strong><br/>
+                            <div style="white-space: pre-wrap;"><?= $comment ?></div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <script>
+            var css = '@page { size: landscape; margin: 8mm; }',
+                    head = document.head || document.getElementsByTagName('head')[0],
+                    style = document.createElement('style');
+            
+            style.type = 'text/css';
+            style.media = 'print';
+            
+            if (style.styleSheet){
+                style.styleSheet.cssText = css;
+            } else {
+                style.appendChild(document.createTextNode(css));
+            }
+            
+            head.appendChild(style);
+            
+            window.print();</script>
+    </body>
+</html>
