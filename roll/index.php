c6595a7d3467539509f98551b6983e0c204a6e21
user <user@Dmitry>
2021-03-10 16:11:33 +0300

    Внешний вид.

--- roll/index.php
+++ roll/index.php
@@ -64,7 +64,9 @@ $total_weight = $row['total_weight'];
                     </table>
                 </div>
                 <div class="p-1">
+                    <?php if(IsInRole(array('technologist', 'dev', 'storekeeper'))): ?>
                     <a href="new.php" class="btn btn-outline-dark" style="padding-top: 14px; padding-bottom: 14px; padding-left: 30px; width: 200px; text-align: left;"><i class="fas fa-plus" style="font-size: 10px; margin-right: 18px;"></i>Новый ролик</a>
+                    <?php endif; ?>
                     <button class="btn btn-outline-dark disabled d-none" data-toggle="modal" data-target="#filterModal" data-text="Фильтр"><img src="../images/icons/filter.svg" style="margin-right: 20px;" />Фильтр</button>
                 </div>
             </div>
